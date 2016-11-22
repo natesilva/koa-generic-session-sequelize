6b018aca3f1708bb674d665ead6f3aef64e7cbf2
Nate Silva <nate@natesilva.com>
2016-11-22 13:37:32 -0800

    add TypeScript definitions

---
+++ index.d.ts
@@ -0,0 +1,29 @@
+//
+// TypeScript definitions for this module.
+//
+
+export = koa_generic_session_sequelize;
+
+declare class koa_generic_session_sequelize {
+    constructor(sequelize: any, options?: koa_generic_session_sequelize.Settings);
+    get(sid: string): Promise<any>;
+    set(sid: string, sess: any, ttl?: number): Promise<any>;
+    destroy(sid: string): Promise<any>;
+}
+
+declare namespace koa_generic_session_sequelize {
+    export interface Settings {
+        /** name of the session table in the db */
+        tableName?: string,
+        /** name of the model to be registered with Sequelize */
+        modelName?: string,
+        /** if true, create the table if it doesn’t exist */
+        sync?: boolean,
+        /** if sync is true, how long to wait, in ms, for sync to complete */
+        syncTimeout?: number,
+        /** do garbage collection approximately every this many requests */
+        gcFrequency?: number,
+        /** if true, the table will have updatedAt and createdAt columns */
+        timestamps?: boolean
+    }
+}
